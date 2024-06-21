714a6da2efd308a1110821a362950804c787770f
Your Name <you@example.com>
2024-06-21 23:31:45 +0200

    initdev

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,17 +1,16 @@
 // sum.js
 function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
-  
-  // __tests__/sum.test.js
-  const sum = require('../sum');
-  
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-  
-  test('adds -1 + 2 to equal 1', () => {
-    expect(sum(-1, 2)).toBe(1);
-  });
-  
+  return a + b;
+}
+module.exports = sum;
+
+// __tests__/sum.test.js
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds -1 + 2 to equal 1', () => {
+  expect(sum(-1, 2)).toBe(1);
+});
