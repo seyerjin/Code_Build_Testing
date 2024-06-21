a02952beec022e446e8bd48b4a3daa6074ceea17
Your Name <you@example.com>
2024-06-21 23:43:50 +0200

    initdev

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,6 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {languageOptions: { globals: globals.browser }}
 ];
