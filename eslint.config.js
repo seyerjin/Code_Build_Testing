84665c6f23a73b2d300e07adf26f6c1387a61942
Your Name <you@example.com>
2024-06-22 00:25:58 +0200

    initdev

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,24 @@
+import globals from 'globals';
+
 export default [
-    {
-      ignores: ["node_modules/**"],
-    },
-    {
-      files: ["**/*.js", "**/*.mjs"],
-      languageOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-      },
-      env: {
-        node: true,
-        es2021: true,
-        jest: true,
-      },
-      rules: {
-        "no-redeclare": "error",
-        "no-undef": "error",
-        "no-unused-vars": "error",
+  {
+    ignores: ["node_modules/**"],
+  },
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+        ...globals.jest,
       },
     },
-  ];
-  
+    rules: {
+      "no-redeclare": "error",
+      "no-undef": "error",
+      "no-unused-vars": "error",
+    },
+  },
+];
